fa283a9095bc76c384e3da32656d5a1b12c5e272
monju777 <83251234+vover11@users.noreply.github.com>
2024-10-17 20:51:53 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./src/**/*.{html,js}",
-  ],
+content: [
+  "./**/*.{html,js}",
+],
   theme: {
     extend: {
         keyframes: {
